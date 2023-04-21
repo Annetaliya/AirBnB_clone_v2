ed0ca93b18f03a384546b9d7330c1bc9e19d3382
Annette Adhiambo <annetaliya@gmail.com>
2023-04-21 01:54:50 -0700

    display route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+''' a script that starts a Flask web application'''
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    '''function that displays hello hbnb'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display():
+    '''A function that returns HBNB'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_diplay(text):
+    '''displays “C ” followed by the value of the text variable'''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
